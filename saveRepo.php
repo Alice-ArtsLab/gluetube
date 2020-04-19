bc429474f726826222728729db342022bc43fdb6
Alice <alice@alice.dcomp.ufsj.edu.br>
2020-04-18 22:00:23 -0300

    Fixed the Store Score functionality

--- saveRepo.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = $_POST['data'];
-echo 'post:\n' . $_POST . " ";
-echo 'data:\n' . $data . " ";
-result = file_put_contents('music_book/' . ((int) microtime(true)) . '.gt', $data, FILE_APPEND | LOCK_EX);
-echo "Sucesso " . result . "\n"
-?>
