4c692a9aa6e734f7055f9c78f1b119c44d750ee6
Alice <alice@alice.dcomp.ufsj.edu.br>
2020-06-07 16:47:19 -0300

    Update the listing and loading scores from server

---
+++ listScores.php
@@ -0,0 +1,18 @@
+<?php
+    $dir = __DIR__ . "/music_book/";
+
+    if ($handle = opendir($dir)) {
+        while ($entry = readdir($handle)) {
+            if ($entry != "." && $entry != "..") {
+                $content = file_get_contents($dir . $entry);
+                $json = json_decode($content, true);
+                echo $json["author"];
+                echo " | ";
+                echo $json["title"];
+                echo "<button onclick='loadScore(\"" . $entry . "\");'>Load</button>";
+                echo "<br/>";
+            }
+        }
+        closedir($handle);
+    }
+?>
