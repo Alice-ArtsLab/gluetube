5af23a401edae26cfe803c0ecf34697f482a3bdf
Lellix <mariplellis6@gmail.com>
2021-08-05 23:03:50 -0300

    update

---
+++ assets/js/gallery/listCategories.js
@@ -0,0 +1,13 @@
+function listCategories() {
+    var e = document.getElementById("list_server_score");
+    var xhr = new XMLHttpRequest();
+    var url = "listCategories.php";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "text/xml");
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            e.innerHTML = xhr.responseText;
+        }
+    }
+    xhr.send(); // Actually execute the request
+}
